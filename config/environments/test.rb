9d4c3aa326b34fab2f784df5d8e6b905de7bcbf0
Keith Woody <keith@synctv.com>
2009-06-16 22:46:09 -0700

    * adds test gem dependencies to test ENV * adds basic databse.yml for sqlite, * sets up cucumber for use with shoulda instead of rspec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -25,3 +25,9 @@ config.action_mailer.delivery_method = :test
 # This is necessary if your schema can't be completely dumped by the schema dumper,
 # like if you have constraints or database-specific column types
 # config.active_record.schema_format = :sql
+
+config.gem "thoughtbot-shoulda", :lib=>false, :version=>"2.10.1", :source=>"http://gems.github.com"
+config.gem "webrat", :lib=>false, :version=>"0.4.4"
+config.gem "cucumber", :lib=>false, :version=>"0.3.11"
+config.gem "thoughtbot-factory_girl", :lib => "factory_girl", :source => "http://gems.github.com"
+config.gem "railroad"
